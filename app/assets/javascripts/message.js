e532cf86ae0dfaea79509093ef461453d2e3436d
nana0707 <hydrangeaheartlessness@gmail.com>
2019-10-29 15:49:11 +0900

    スクロール 修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,17 +61,19 @@ $(function(){
           data: {id: last_message_id}
         })
         .done(function(messages) {
-          var insertHTML = '';
-          messages.forEach(function (message) {
-          insertHTML = buildHTML(message);
+          var insertHTML = ``; 
+          messages.forEach(function(message) {
+            if (message.id >= last_message_id ) {
+              insertHTML == buildHTML(message);
+            }
           $('.messages').append(insertHTML); 
-          $('div').animate({scrollTop: $('.messages').height()})
-          })
+          $('.messages').stop().animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          });
         })
         .fail(function() {
           alert('更新に失敗しました');
-        });
-      };
+        })
+      }
     }
     setInterval(reloadMessages, 5000);
 });
